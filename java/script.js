2003afa102d8a21b5078df9813d45e5157137930
larisssa <140644842+Larissadelara@users.noreply.github.com>
2025-10-22 08:05:14 -0300

    vdgf

---
+++ java/script.js
@@ -0,0 +1,7 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultados = document.querySelector(".caixa-resultados");
+const textoResultados = document.querySelector(".texto-resultados");
+const botaoJogarNovamento = document.querySelector(".novamente-botao");
+const telaInicial = document.querySelector(".tela-inicial");
