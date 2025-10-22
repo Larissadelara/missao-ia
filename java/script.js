a7215ba702bb51bb7201176864c81fe35c1e727d
larisssa <140644842+Larissadelara@users.noreply.github.com>
2025-10-22 14:20:49 -0300

    ,jhilhioyuphfurdyuroduhetsihuq4tiuyfyu

--- java/script.js
+++ java/script.js
@@ -36,5 +36,7 @@ function mostraPergunta (){
 function mostraAlternativas(){
     for (const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent =alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
     }
 }
