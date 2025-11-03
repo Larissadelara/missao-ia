1bef30cb115a67fd34030073f2dcd01bec72172c
larisssa <140644842+Larissadelara@users.noreply.github.com>
2025-11-03 09:47:45 -0300

    rgh

---
+++ java/perguntas.js
@@ -0,0 +1,10 @@
+export const perguntas =[
+    {
+    enunciado: "O país está em uma encruzilhada moral e econômica. Análises recentes revelam que o coeficiente de Gini (medida de desigualdade) atingiu seu pico histórico, e a baixa taxa de mobilidade social sugere que as crianças nascidas nas famílias mais pobres têm chances quase nulas de alcançar o patamar de renda dos mais ricos. Essa disparidade crescente ameaça a estabilidade democrática e a sustentabilidade do crescimento a longo prazo. O governo deve tomar uma decisão imediata sobre a direção a seguir."
+alternativas:[
+    {
+        texto: "O país está em uma encruzilhada moral e econômica. Análises recentes revelam que o coeficiente de Gini (medida de desigualdade) atingiu seu pico histórico, e a baixa taxa de mobilidade social sugere que as crianças nascidas nas famílias mais pobres têm chances quase nulas de alcançar o patamar de renda dos mais ricos. Essa disparidade crescente ameaça a estabilidade democrática e a sustentabilidade do crescimento a longo prazo. O governo deve tomar uma decisão imediata sobre a direção a seguir."
+        afirmacao: "A nação deve priorizar a Justiça Estrutural (Alternativa 1) por meio de políticas de equidade ousadas, como a desoneração de impostos sobre itens essenciais e o aumento da tributação sobre grandes patrimônios, investindo integralmente os recursos em Saneamento Básico, Habitação Popular e cotas educacionais."
+       ],
+    }
+]
