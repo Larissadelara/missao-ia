be8a6ddbc3696a1b2738925b95f41f90b67840fd
larisssa <140644842+Larissadelara@users.noreply.github.com>
2025-11-05 14:17:24 -0300

    bhj

--- java/perguntas.js
+++ java/perguntas.js
@@ -1,10 +1,36 @@
-export const perguntas =[
+export const perguntas = [
     {
-    enunciado: "O país está em uma encruzilhada moral e econômica. Análises recentes revelam que o coeficiente de Gini (medida de desigualdade) atingiu seu pico histórico, e a baixa taxa de mobilidade social sugere que as crianças nascidas nas famílias mais pobres têm chances quase nulas de alcançar o patamar de renda dos mais ricos. Essa disparidade crescente ameaça a estabilidade democrática e a sustentabilidade do crescimento a longo prazo. O governo deve tomar uma decisão imediata sobre a direção a seguir."
+    enunciado:"O país está em uma encruzilhada moral e econômica. Análises recentes revelam que o coeficiente de Gini (medida de desigualdade) atingiu seu pico histórico, e a baixa taxa de mobilidade social sugere que as crianças nascidas nas famílias mais pobres têm chances quase nulas de alcançar o patamar de renda dos mais ricos. Essa disparidade crescente ameaça a estabilidade democrática e a sustentabilidade do crescimento a longo prazo. O governo deve tomar uma decisão imediata sobre a direção a seguir.",
 alternativas:[
-    {
-        texto: "O país está em uma encruzilhada moral e econômica. Análises recentes revelam que o coeficiente de Gini (medida de desigualdade) atingiu seu pico histórico, e a baixa taxa de mobilidade social sugere que as crianças nascidas nas famílias mais pobres têm chances quase nulas de alcançar o patamar de renda dos mais ricos. Essa disparidade crescente ameaça a estabilidade democrática e a sustentabilidade do crescimento a longo prazo. O governo deve tomar uma decisão imediata sobre a direção a seguir."
-        afirmacao: "A nação deve priorizar a Justiça Estrutural (Alternativa 1) por meio de políticas de equidade ousadas, como a desoneração de impostos sobre itens essenciais e o aumento da tributação sobre grandes patrimônios, investindo integralmente os recursos em Saneamento Básico, Habitação Popular e cotas educacionais."
+        {
+        texto:"O país está em uma encruzilhada moral e econômica. Análises recentes revelam que o coeficiente de Gini (medida de desigualdade) atingiu seu pico histórico, e a baixa taxa de mobilidade social sugere que as crianças nascidas nas famílias mais pobres têm chances quase nulas de alcançar o patamar de renda dos mais ricos. Essa disparidade crescente ameaça a estabilidade democrática e a sustentabilidade do crescimento a longo prazo. O governo deve tomar uma decisão imediata sobre a direção a seguir.",
+        afirmacao:[
+            "A nação deve priorizar a Justiça Estrutural (Alternativa 1) por meio de políticas de equidade ousadas, como a desoneração de impostos sobre itens essenciais e o aumento da tributação sobre grandes patrimônios, investindo integralmente os recursos em Saneamento Básico, Habitação Popular e cotas educacionais."
        ],
+       proxima: 1,
+    },
+    {
+        texto:"O governo decide não alterar a estrutura tributária e foca em cortes de gastos públicos não essenciais (incluindo programas sociais considerados assistencialistas e em incentivos fiscais massivos para grandes corporações e investidores internacionais, sob a crença de que isso impulsionará rapidamente o PIB e gerará empregos. A abordagem ignora a urgência das disparidades sociais",
+        afirmação:[
+            "A nação deve priorizar o Crescimento Imediato (Alternativa 2), abstendo-se de alterar a estrutura tributária redistributiva e focando em incentivos fiscais para o capital e cortes de gastos, acreditando que o mercado livre é o motor mais eficiente para o desenvolvimento econômico",
+        ],
+        proxima: 2,
+    }
+]
+    },
+    {
+        enunciado:
+        alternativas:[
+            {
+                texto:
+                afirmacao:[
+
+                ],
+                proxima:3,
+            },
+            {
+            texto
+        ]
+        
     }
 ]
