289912068458bc75bf995984ac6fb0ed211ed044
larisssa <140644842+Larissadelara@users.noreply.github.com>
2025-12-01 09:36:03 -0300

    fc

--- java/aleatorio.js
+++ java/aleatorio.js
@@ -1,6 +1,6 @@
 const nomes = ["Larissa", "Gabrieli", "Eduardo", "Angela", "Rodrigo", "Poliana" ]
 export function aleatorio (lista){
-    const posicao = Math.florr(Math.random()*lista.lenght);
+    const posicao = Math.floor(Math.random()*lista.lenght);
     return lista [posicao];
 }
 export const nome = aleatorio(nomes)
